cf875de17d577ca4f92b0bcbf35c9a07d74cc2da
Suike <Suike@macbookpro.home>
2025-06-17 06:39:05 +0100

    blank space

--- back_end/models/yacht/src/models/base_yacht/database.py
+++ back_end/models/yacht/src/models/base_yacht/database.py
@@ -196,7 +196,7 @@ class BaseYachtDatabase:
         ''')
         self.conn.commit()
         return True
-    
+
     def get_yacht(self, yacht_id):
         cursor = self.conn.cursor()
         cursor.execute('SELECT * FROM yachts WHERE id = ?', (yacht_id,))
